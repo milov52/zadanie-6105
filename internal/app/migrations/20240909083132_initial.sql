7db6ec6a82b01a75c19c0f1e742fd579d2889522
Milov Aleksey <milov52@gmail.com>
2024-09-12 20:05:14 +0300

    added migration file

---
+++ internal/app/migrations/20240909083132_initial.sql
@@ -0,0 +1,97 @@
+-- +goose Up
+-- CREATE TABLE IF NOT EXISTS employee (
+--                           id SERIAL PRIMARY KEY,
+--                           username VARCHAR(50) UNIQUE NOT NULL,
+--                           first_name VARCHAR(50),
+--                           last_name VARCHAR(50),
+--                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--                           updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+--
+-- CREATE TYPE organization_type AS ENUM (
+--     'IE',
+--     'LLC',
+--     'JSC'
+--     );
+--
+-- CREATE TABLE IF NOT EXISTS organization (
+--                               id SERIAL PRIMARY KEY,
+--                               name VARCHAR(100) NOT NULL,
+--                               description TEXT,
+--                               type organization_type,
+--                               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--                               updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+--
+-- CREATE TABLE IF NOT EXISTS organization_responsible (
+--                                           id SERIAL PRIMARY KEY,
+--                                           organization_id INT REFERENCES organization(id) ON DELETE CASCADE,
+--                                           user_id INT REFERENCES employee(id) ON DELETE CASCADE
+-- );
+--
+-- CREATE TYPE service_type AS ENUM (
+--     'Construction',
+--     'Delivery',
+--     'Manufacture'
+--     );
+--
+-- CREATE TYPE tender_status AS ENUM (
+--     'Created',
+--     'Published',
+--     'Closed'
+--     );
+--
+-- CREATE TABLE IF NOT EXISTS tender (
+--                         id SERIAL PRIMARY KEY ,
+--                         name VARCHAR(100) NOT NULL,
+--                         description VARCHAR(500) NOT NULL,
+--                         service_type service_type,
+--                         status tender_status DEFAULT 'Created',
+--                         organization_id INT REFERENCES organization(id) ON DELETE CASCADE,
+--                         user_id INT  REFERENCES employee(id) ON DELETE CASCADE,
+--                         version INT DEFAULT 1,
+--                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+--
+-- CREATE TYPE bild_status AS ENUM (
+--     'Created',
+--     'Published',
+--     'Canceled',
+--     'Approved',
+--     'Rejected'
+--     );
+--
+-- CREATE TYPE author_type AS ENUM (
+--     'Created',
+--     'Published',
+--     'Closed'
+--     );
+
+CREATE TABLE IF NOT EXISTS bid (
+                      id SERIAL PRIMARY KEY ,
+                      name VARCHAR(100) NOT NULL,
+                      description VARCHAR(500) NOT NULL,
+                      status bild_status,
+                      tender_id INT REFERENCES tender(id) ON DELETE CASCADE,
+                      author_type author_type,
+                      author_id INT REFERENCES employee(id) ON DELETE CASCADE,
+                      version INT DEFAULT 1,
+                      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- +goose Down
+-- Удаляем зависимые таблицы
+-- DROP TABLE IF EXISTS organization_responsible;
+DROP TABLE IF EXISTS bild;
+-- DROP TABLE IF EXISTS tender;
+-- DROP TABLE IF EXISTS employee;
+-- DROP TABLE IF EXISTS organization;
+
+-- Удаляем типы, когда они больше не используются
+-- DROP TYPE IF EXISTS organization_type;
+-- DROP TYPE IF EXISTS service_type;
+-- DROP TYPE IF EXISTS tender_status;
+DROP TYPE IF EXISTS bid_status;
+-- DROP TYPE IF EXISTS author_type;
+
+
